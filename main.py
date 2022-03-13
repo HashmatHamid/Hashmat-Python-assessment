6342e089dcaaabcbff7e42903a461872e4dc1bff
Hashmat Hamid <>
2022-03-13 23:37:41 +0000

    print("Hello and welcome to my general mathematics quiz, I hope you enjoy this quiz") name=input("What is your name? ")
    print("Hello {} I hope you are well today".format (name))

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello and welcome to my general mathematics quiz, I hope you enjoy this quiz")
+name=input("What is your name? ")
+ 
+print("Hello {} I hope you are well today".format (name))
