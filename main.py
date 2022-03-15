b74fc34dc3baa4b2b59979072033205db68793cd
Hashmat Hamid <100541723+HashmatHamid@users.noreply.github.com>
2022-03-15 20:55:20 +0000

    print("Hello and welcome to my general mathematics quiz. In this quiz you will be asked from 10-50 mathematics questions based on your choice. Also depending on your choice, you can choose to  do either a hard or easy quiz. The hard quiz will feature algebra and number, you will also  be only allowed to answer with word answers. With the easy questions you will be asked number questions and the answers wil be in a multi-choice form. I reccomend doing a playthrough of both the quizes at least once. ") print("I hope you enjoy :)")

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-print("Hello and welcome to my general mathematics quiz, I hope you enjoy this quiz")
+print("Hello and welcome to my general mathematics quiz. In this quiz you will be asked from 10-50 mathematics questions based on your choice. Also depending on your choice, you can choose to  do either a hard or easy quiz. The hard quiz will feature algebra and number, you will also  be only allowed to answer with word answers. With the easy questions you will be asked number questions and the answers wil be in a multi-choice form. I reccomend doing a playthrough of both the quizes at least once. ")
+print("I hope you enjoy :)")
 name=input("What is your name? ")
+if name == int:
+  print("qq")
+else: print("Hello {} I hope you have fun playing my quiz".format(name))
 if name == str( ): 
   print("That was not a name")
   name_1=input("What is really your name? ")
   print("Hello {} I hope you have fun playing my game".format(name_1))
+ 
 else: 
   print("Hello {}, I hope you enjoy my quiz".format(name))
