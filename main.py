d2da5baad373adf1e3ee0492b23f92b362f402d7
Hashmat Hamid <100541723+HashmatHamid@users.noreply.github.com>
2022-03-15 21:00:36 +0000

    print("""Hello and welcome to my general mathematics quiz. In this quiz you will be asked from 10-50 mathematics questions based on your choice. Also depending on your choice, you can choose to  do either a hard or easy quiz. The hard quiz will feature algebra and number, you will also  be only allowed to answer with word answers. With the easy questions you will be asked number questions and the answers wil be in a multi-choice form. I reccomend doing a playthrough of both the quizes at least once. """) print("I hope you enjoy :)")

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello and welcome to my general mathematics quiz. In this quiz you will be asked from 10-50 mathematics questions based on your choice. Also depending on your choice, you can choose to  do either a hard or easy quiz. The hard quiz will feature algebra and number, you will also  be only allowed to answer with word answers. With the easy questions you will be asked number questions and the answers wil be in a multi-choice form. I reccomend doing a playthrough of both the quizes at least once. ")
+print("""Hello and welcome to my general mathematics quiz.
+In this quiz you will be asked from 10-50 mathematics questions based on your choice.
+Also depending on your choice, you can choose to  do either a hard or easy quiz.
+The hard quiz will feature algebra and number, you will also  be only allowed to answer with word answers. 
+With the easy questions you will be asked number questions and the answers wil be in a multi-choice form. 
+I reccomend doing a playthrough of both the quizes at least once. """)
 print("I hope you enjoy :)")
 name=input("What is your name? ")
 if name == int:
